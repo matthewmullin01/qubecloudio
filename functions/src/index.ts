30440e50c9b6ad7a51eb2ffc271b28159c2c5e39
Matthew Mullin <matthewmullin01@gmail.com>
2020-05-22 21:14:53 +0200

    Audit v0.5

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,13 @@
 import * as admin from 'firebase-admin';
 
 import { createServer as _createServer } from './cloudvm/create-server';
+import { auditCron as _auditCron } from './audits/audit';
 import { serverListener as _serverListener } from './cloudvm/server-listener';
 import { handlePaddleWebook as _handlePaddleWebook } from './paddle/handle-paddle-webook';
 
 admin.initializeApp();
 
 export const createServer = _createServer;
+export const auditCron2 = _auditCron;
 export const serverListener = _serverListener;
 export const handlePaddleWebook = _handlePaddleWebook;
