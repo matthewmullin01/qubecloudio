2b9827ea580e372dfbc0b71f8d6c6b6051041716
Matthew Mullin <matthew.mullin@zenaptix.com>
2020-05-29 09:27:27 +0200

    Analytics v0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
-import { User } from 'firebase';
+import { User, analytics } from 'firebase';
 
 @Component({
   selector: 'app-root',
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDEIqGattV-AjD1qShNgctNw2j8jQclMu0',
+    authDomain: 'minecraft-63461.firebaseapp.com',
+    databaseURL: 'https://minecraft-63461.firebaseio.com',
+    projectId: 'minecraft-63461',
+    storageBucket: 'minecraft-63461.appspot.com',
+    messagingSenderId: '962302234098',
+    appId: '1:962302234098:web:a395503c11a2213ce3ef6b',
+    measurementId: 'G-P0CFHWM402',
+  },
 };
